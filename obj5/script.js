debc31fc6d62803ce7023d06ff312661a517bc14
new <gaybullayevamrayhona718@gmail.com>
2024-08-28 18:28:35 +0500

    obj5

---
+++ obj5/script.js
@@ -0,0 +1,23 @@
+function chiqar() {
+    const obj = {
+        id: 1,
+        name: "atirgul",
+        color: "qizil",
+        count: 10,
+        price: "10$"
+    }
+
+    let stringValue = {}
+
+    for (let key in obj) {
+        if (typeof obj[key] === "string") {
+            stringValue[key] = obj[key]
+        }
+    }
+    console.log(stringValue.valueOf())
+
+}
+
+chiqar()
+
+
