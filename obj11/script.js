c2e92ed7eead38abc7fda03a74a039b98df5a06a
new <gaybullayevamrayhona718@gmail.com>
2024-08-28 22:49:19 +0500

    obj11

---
+++ obj11/script.js
@@ -0,0 +1,23 @@
+function chiqar() {
+    let obj = {
+        id: 1,
+        name: 'Usmon',
+        job: 'developer',
+        practice: 1
+    }
+
+    if (obj.practice == 1) {
+        console.log("qabul qilindi")
+    }
+
+    else {
+        console.log("ming bor uzr")
+    }
+}
+chiqar()
+
+
+
+
+
+
