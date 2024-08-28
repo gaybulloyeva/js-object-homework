98674096817a4251f049b27472558201fa06f6a8
new <gaybullayevamrayhona718@gmail.com>
2024-08-28 22:54:30 +0500

    obj12

---
+++ obj12/script.js
@@ -0,0 +1,26 @@
+function chiqar() {
+    let obj = {
+        id: 1,
+        name: 'Usmon',
+        job: 'developer',
+        practice: 1,
+        talaba: true
+    }
+
+    if (obj.talaba) {
+        obj.kredit = true
+    }
+
+    else {
+        obj.kredit = "olinmadi"
+    }
+
+    console.log(obj)
+}
+chiqar()
+
+
+
+
+
+
