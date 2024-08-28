f5ef08fd84916d11bae7858a0546f553896bef33
new <gaybullayevamrayhona718@gmail.com>
2024-08-28 18:02:44 +0500

    obj2

---
+++ obj2/script.js
@@ -0,0 +1,20 @@
+function chiqar() {
+    const obj = {
+        id: 1,
+        name: "IT COMPANY"
+    }
+    if (obj.id == Number) {
+        console.log(obj.id)
+    }
+
+    else if (obj.name == Number) {
+        console.log(obj.name)
+    }
+
+    else {
+        `object valuelarining raqamga tengi yo'q`
+    }
+
+}
+
+chiqar()
